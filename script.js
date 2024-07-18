c08aa8ed7f083e577b4a88859f650a4a0837c0d6
Njolul <alexanderolvea@gmail.com>
2024-07-18 09:57:19 -0400

    3

--- script.js
+++ script.js
@@ -16,7 +16,14 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         return;
     }
 
-    // Si la validación es exitosa, puedes proceder a enviar los datos
-    // Aquí puedes agregar tu lógica de envío de datos, por ejemplo, usando AJAX
-    alert('Inicio de sesión exitoso');
+    // Simulando inicio de sesión exitoso (aquí deberías realizar la autenticación real)
+    var loggedIn = true; // Cambia a false si la autenticación falla
+
+    if (loggedIn) {
+        // Redirigir a otra página
+        window.location.href = 'index.html'; // Cambia 'dashboard.html' por la URL de la página a la que quieres redirigir
+    } else {
+        alert('Inicio de sesión fallido. Por favor, verifica tus credenciales.');
+    }
 });
+
